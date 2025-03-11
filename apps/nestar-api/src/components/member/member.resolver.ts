7409317cff5a2428594a834662a559164342c2ae
Felix0308 <utkirbekm.0327@gmail.com>
2025-03-11 21:36:59 +0900

    feat: develop login business logic

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -21,13 +21,12 @@ export class MemberResolver {
 		}
 	}
 
-	@Mutation(() => String)
+	@Mutation(() => Member)
 	@UsePipes(ValidationPipe) // kirib kelayotgan malumotlarni tekshirish tizimi
-	public async login(@Args('input') input: LoginInput): Promise<string> {
-
+	public async login(@Args('input') input: LoginInput): Promise<Member> {
 		try {
 			console.log('Mutation: login');
-			return this.memberService.login();
+			return this.memberService.login(input);
 		} catch (err) {
 			console.log('Error, signup:', err);
 			throw new InternalServerErrorException(err);
