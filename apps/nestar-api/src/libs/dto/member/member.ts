bd1ec5f4b4030fe6156b03de61eb0ae1d2227a9f
Felix0308 <utkirbekm.0327@gmail.com>
2025-03-18 19:53:44 +0900

    feat: develop getAgents GraphQL api

--- apps/nestar-api/src/libs/dto/member/member.ts
+++ apps/nestar-api/src/libs/dto/member/member.ts
@@ -80,4 +80,20 @@ export class Member {
 
 	@Field(() => String, { nullable: true })
 	accessToken?: String;
+
+}
+
+@ObjectType()
+export class TotalCounter {
+	@Field(() => Int, { nullable: true })
+	total?: number;
+}
+
+@ObjectType()
+export class Members {
+	@Field(() => [Member])
+	list: Member[];
+
+	@Field(() => [TotalCounter], { nullable: true })
+	metaCounter?: TotalCounter[];
 }
