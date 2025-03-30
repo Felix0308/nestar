e7ac7a9cbfbde8ac77cff473ec0245302ef196d6
Felix0308 <utkirbekm.0327@gmail.com>
2025-03-30 17:27:00 +0900

    feta: develop createComment, updateComment and getComment and getComments GraphQL APIs

--- apps/nestar-api/src/components/comment/comment.resolver.ts
+++ apps/nestar-api/src/components/comment/comment.resolver.ts
@@ -14,4 +14,39 @@ import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Resolver()
-export class CommentResolver {}
+export class CommentResolver {
+	constructor(private readonly commentService: CommentService) {}
+    //commentService ni instance ni hosil qildik
+
+	@UseGuards(AuthGuard)
+	@Mutation((returns) => Comment)
+	public async createComment(
+		@Args('input') input: CommentInput,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Comment> {
+		console.log('Mutation: createComment');
+		return await this.commentService.createComment(memberId, input);
+	}
+
+	@UseGuards(AuthGuard)
+	@Mutation((returns) => Comment)
+	public async updateComment(
+		@Args('input') input: CommentUpdate,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Comment> {
+		console.log('Mutation: updateComment');
+		input._id = shapeIntoMongoObjectId(input._id);
+		return await this.commentService.updateComment(memberId, input);
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query((returns) => Comments)
+	public async getComments(
+		@Args('input') input: CommentsInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Comments> {
+		console.log('Query: getComments');
+		input.search.commentRefId = shapeIntoMongoObjectId(input.search.commentRefId);
+		return await this.commentService.getComments(memberId, input);
+	}
+}
--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -13,4 +13,97 @@ import { T } from '../../libs/types/common';
 import { lookupMember } from '../../libs/config';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+	constructor(
+		@InjectModel('Comment') private readonly commentModel: Model<Comment>,
+		// comment schime moduleni injection qildik
+		private readonly memberService: MemberService, // instance lari olinyapti
+		private readonly boardArticleService: BoardArticleService,
+		private readonly propertyService: PropertyService,
+	) {}
+
+	public async createComment(memberId: ObjectId, input: CommentInput): Promise<Comment> {
+		input.memberId = memberId; // murojatchini memberIdsini inputga joyladik
+		let result: Comment | null = null;
+
+		try {
+			result = await this.commentModel.create(input);
+		} catch (err) {
+			console.log('Error, Service.model:', err.message);
+			throw new BadRequestException(Message.CREATE_FAILED);
+		}
+
+		switch (input.commentGroup) {
+			case CommentGroup.PROPERTY:
+				await this.propertyService.propertyStatsEditor({
+					_id: input.commentRefId,
+					targetKey: 'propertyComments',
+					modifier: 1,
+				});
+				break;
+
+			case CommentGroup.MEMBER:
+				await this.memberService.memberStatsEditor({
+					_id: input.commentRefId,
+					targetKey: 'memberComments',
+					modifier: 1,
+				});
+				break;
+
+			case CommentGroup.ARTICLE:
+				await this.boardArticleService.boardArticleStatsEditor({
+					_id: input.commentRefId,
+					targetKey: 'articleComments',
+					modifier: 1,
+				});
+				break;
+		}
+
+		if (!result) throw new InternalServerErrorException(Message.CREATE_FAILED);
+		return result;
+	}
+
+	public async updateComment(memberId: ObjectId, input: CommentUpdate): Promise<Comment> {
+		const { _id } = input;
+		const result = await this.commentModel.findOneAndUpdate(
+			{
+				_id: _id, // comment id si
+				memberId: memberId, // murojatchi id si
+				commentStatus: CommentStatus.ACTIVE,
+			}, // comment statusi ACTIVE
+			input,
+			{ new: true },
+		);
+
+		if (!result) throw new BadRequestException(Message.UPDATE_FAILED);
+		return result;
+	}
+
+	public async getComments(memberId: ObjectId, input: CommentsInquiry): Promise<Comments> {
+		const { commentRefId } = input.search;  // aynan comment reference id ga daxldor commentlarni ko'rmoqchimiz
+		const match: T = { commentRefId: commentRefId, commentStatus: CommentStatus.ACTIVE };
+		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
+
+		const result = await this.commentModel
+			.aggregate([
+				{ $match: match },
+				{ $sort: sort },
+				{
+					$facet: {
+						list: [
+							{ $skip: (input.page - 1) * input.limit },
+							{ $limit: input.limit },
+							lookupMember,
+							{ $unwind: '$memberData' },
+						],
+						metaCounter: [{ $count: 'total' }],  // comment larni umumiy soni
+					},
+				},
+			])
+			.exec();
+
+		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		return result[0];
+	}
+}
