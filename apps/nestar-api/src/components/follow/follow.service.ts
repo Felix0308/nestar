e2a15d564816a4d19a694b1d181281243ddc4a3c
Felix0308 <utkirbekm.0327@gmail.com>
2025-04-06 16:04:01 +0900

    fix: modify like interaction realted logic

--- apps/nestar-api/src/components/follow/follow.service.ts
+++ apps/nestar-api/src/components/follow/follow.service.ts
@@ -6,7 +6,7 @@ import { MemberService } from '../member/member.service';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { FollowInquiry } from '../../libs/dto/follow/follow.input';
 import { T } from '../../libs/types/common';
-import { lookupFollowerData, lookupFollowingData } from '../../libs/config';
+import { lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
 
 @Injectable()
 export class FollowService {
@@ -16,17 +16,17 @@ export class FollowService {
 	) {}
 
 	public async subscribe(followerId: ObjectId, followingId: ObjectId): Promise<Follower> {
-		if (followerId.toString() === followingId.toString()) {  
-            // har ikkla objectID ni stringga o'tkazib solishtiryapmiz, 
-            // referencelari boshqa bo'lganligi uchun to'g'ridan to'g'ri qiymatlarini solishtira olmaymiz
+		if (followerId.toString() === followingId.toString()) {
+			// har ikkla objectID ni stringga o'tkazib solishtiryapmiz,
+			// referencelari boshqa bo'lganligi uchun to'g'ridan to'g'ri qiymatlarini solishtira olmaymiz
 			throw new InternalServerErrorException(Message.SELF_SUBSCRIPTION_DENIED);
 		}
 
-		const targetMember = await this.memberService.getMember(null, followingId); 
-        // detail pageda emasligi uchun birinchi murojatchimiz null bo'ladui
+		const targetMember = await this.memberService.getMember(null, followingId);
+		// detail pageda emasligi uchun birinchi murojatchimiz null bo'ladui
 		if (!targetMember) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
 
-		const result = await this.registerSubscription(followerId, followingId);  // ro'yhatga olish
+		const result = await this.registerSubscription(followerId, followingId); // ro'yhatga olish
 
 		await this.memberService.memberStatsEditor({ _id: followerId, targetKey: 'memberFollowings', modifier: 1 });
 		await this.memberService.memberStatsEditor({ _id: followingId, targetKey: 'memberFollowers', modifier: 1 });
@@ -36,10 +36,10 @@ export class FollowService {
 
 	private async registerSubscription(followerId: ObjectId, followingId: ObjectId): Promise<Follower> {
 		try {
-			return await this.followModel.create({ 
-                followingId: followingId, 
-                followerId: followerId 
-            });
+			return await this.followModel.create({
+				followingId: followingId,
+				followerId: followerId,
+			});
 		} catch (err) {
 			console.log('Error, Service.model', err);
 			throw new BadRequestException(Message.CREATE_FAILED);
@@ -50,14 +50,14 @@ export class FollowService {
 		const targetMember = await this.memberService.getMember(null, followingId);
 		if (!targetMember) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
 
-		const result = await this.followModel.findOneAndDelete({ 
-            followingId: followingId, 
-            followerId: followerId 
-        });
+		const result = await this.followModel.findOneAndDelete({
+			followingId: followingId,
+			followerId: followerId,
+		});
 		if (!result) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
 
 		await this.memberService.memberStatsEditor({ _id: followerId, targetKey: 'memberFollowings', modifier: -1 });
-        // birlik argumentni path qilyapmiz
+		// birlik argumentni path qilyapmiz
 		await this.memberService.memberStatsEditor({ _id: followingId, targetKey: 'memberFollowers', modifier: -1 });
 
 		return result;
@@ -66,7 +66,7 @@ export class FollowService {
 	public async getMemberFollowings(memberId: ObjectId | null, input: FollowInquiry): Promise<Followings> {
 		const { page, limit, search } = input;
 		if (!search?.followerId) throw new InternalServerErrorException(Message.BAD_REQUEST);
-        // agar followerId null ni tashkil etgan bo'lsa (fronteddan yuborilmagan bo'lsa), backend validationni amalga oshirdik
+		// agar followerId null ni tashkil etgan bo'lsa (fronteddan yuborilmagan bo'lsa), backend validationni amalga oshirdik
 
 		const match: T = { followerId: search?.followerId };
 		console.log('match', match);
@@ -74,16 +74,17 @@ export class FollowService {
 		const result = await this.followModel
 			.aggregate([
 				{ $match: match },
-				{ $sort: { created: Direction.DESC } },  // Direction Decending ko'rinishida bo'lyapti
+				{ $sort: { created: Direction.DESC } }, // Direction Decending ko'rinishida bo'lyapti
 				{
-					$facet: {  // faced orqali hozirgi pipeline 2 ta mustaqil pipeline ga ajratib bir vaqtning o'zida ikkala pipelinedan foydalanish imkonini beradi
+					$facet: {
+						// faced orqali hozirgi pipeline 2 ta mustaqil pipeline ga ajratib bir vaqtning o'zida ikkala pipelinedan foydalanish imkonini beradi
 						list: [
 							{ $skip: (page - 1) * limit },
 							{ $limit: limit },
-                            // meLiked
-                            // meFollowed
+							lookupAuthMemberLiked(memberId, '$followingId'),
+							// meFollowed
 							lookupFollowingData,
-							{ $unwind: '$followingData' },  // following bo'lgan memberni to'liq malumotini olib beradi
+							{ $unwind: '$followingData' }, // following bo'lgan memberni to'liq malumotini olib beradi
 						],
 						metaCounter: [{ $count: 'total' }],
 					},
@@ -99,12 +100,13 @@ export class FollowService {
 		const { page, limit, search } = input;
 		if (!search?.followingId) throw new InternalServerErrorException(Message.BAD_REQUEST);
 
-		const match: T = { followingId: search?.followingId }; 
-        // followingId ni search?.followingId ga teng bo'lgandagi qiymatini matchga tenglayabmiz.
+		const match: T = { followingId: search?.followingId };
+		// followingId ni search?.followingId ga teng bo'lgandagi qiymatini matchga tenglayabmiz.
 		console.log('match', match);
 
 		const result = await this.followModel
-			.aggregate([  // database server ni maydonida hosil bo'ladi
+			.aggregate([
+				// database server ni maydonida hosil bo'ladi
 				{ $match: match },
 				{ $sort: { created: Direction.DESC } },
 				{
@@ -112,7 +114,8 @@ export class FollowService {
 						list: [
 							{ $skip: (page - 1) * limit },
 							{ $limit: limit },
-							// meLiked
+							lookupAuthMemberLiked(memberId, '$followerId'),
+
 							// meFollowed
 							lookupFollowerData,
 							{ $unwind: '$followerData' },
