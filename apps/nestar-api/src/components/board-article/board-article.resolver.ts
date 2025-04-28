696b88fba8d9c60096ce838bd15136f218844c6b
Felix0308 <utkirbekm.0327@gmail.com>
2025-04-28 17:44:43 +0900

    fix: fixed is articleID => articleId in board-article file

--- apps/nestar-api/src/components/board-article/board-article.resolver.ts
+++ apps/nestar-api/src/components/board-article/board-article.resolver.ts
@@ -34,7 +34,7 @@ export class BoardArticleResolver {
 	@UseGuards(WithoutGuard)
 	@Query((returns) => BoardArticle) // BoardArticle dto bn malumot qaytaradi
 	public async getBoardArticle(
-		@Args('articleID') input: string,
+		@Args('articleId') input: string,
 		@AuthMember('_id') memberId: ObjectId | null,
 	): Promise<BoardArticle> {
 		console.log('Query: getBoardArticle');
@@ -105,7 +105,7 @@ export class BoardArticleResolver {
 		@AuthMember('_id') memberId: ObjectId,
 	): Promise<BoardArticle> {
 		console.log('Mutation: removeBoardArticleByAdmin');
-		const articleID = shapeIntoMongoObjectId(input);
-		return await this.boardArticleService.removeBoardArticleByAdmin(articleID);
+		const articleId = shapeIntoMongoObjectId(input);
+		return await this.boardArticleService.removeBoardArticleByAdmin(articleId);
 	}
 }
